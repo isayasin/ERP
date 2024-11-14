4dfccfef54c9d34a0b26876a36c21bb164b9b7ef
isayasin <isayasinkuru@gmail.com>
2024-11-15 01:12:24 +0300

    Set up Swal Service(CallToast and CallSwal)

--- ERPClient/src/app/components/home/home.component.ts
+++ ERPClient/src/app/components/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { BlankComponent } from '../blank/blank.component';
-import { SectionComponent } from '../section/section.component';
 import { SharedModule } from '../../modules/shared.module';
 
 @Component({
